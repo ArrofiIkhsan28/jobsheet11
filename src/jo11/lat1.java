37f1c1c2acdfc272d32d3a8525889821b0debdaa
Hp <Hp@DESKTOP-GFENN21>
2018-08-14 11:15:12 +0700

    Initial Commit

---
+++ src/jo11/lat1.java
@@ -0,0 +1,11 @@
+
+package jo11;
+import java.util.Locale;
+public class lat1 {
+    public static void main(String[] args){
+       String identitas = "Arrofi Ikhsan Nur Abror / X RPL 2 / 8";
+        System.out.println("identitas : " + identitas);
+        
+         
+    
+}
