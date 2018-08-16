44978f312249877f767bd0c352c839c13a6fa09f
Hp <Hp@DESKTOP-GFENN21>
2018-08-16 22:20:58 +0700

    Coba method contains, toUpperCase, toLowerCase, trim & concat.

--- src/jo11/lat1.java
+++ src/jo11/lat1.java
@@ -39,6 +39,15 @@ public class lat1 {
         System.out.println("isi variable s :" +s);
         System.out.println("perbandingan isi r dengan s (case sensitive) :"+r.compareTo(s));
         
+    System.out.println("pada x terdapat huruf'a' : " + x.contains("a"));
          
+         System.out.println("isi variable x besar semua :"+x.toUpperCase());
+         System.out.println("isi semua variable  x kecil semua : "+x.toLowerCase());
+         
+         String t = "  operasi string  ";
+         System.out.println("isi variable t:\""+ t +"\"");
+         System.out.println("isi variable t tanpa spasi : \""+t.trim() +"\"");
+         
+         System.out.println("gabungkan isi variable x dan r : "+ x.concat(r));
     }
 }
