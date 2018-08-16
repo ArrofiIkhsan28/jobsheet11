d8c28dd5d28d9aba7632d6de9a9a772e652659f0
Hp <Hp@DESKTOP-GFENN21>
2018-08-16 18:27:52 +0700

    coba method length &isEMPTY

--- src/jo11/lat1.java
+++ src/jo11/lat1.java
@@ -5,7 +5,16 @@ public class lat1 {
     public static void main(String[] args){
        String identitas = "Arrofi Ikhsan Nur Abror / X RPL 2 / 8";
         System.out.println("identitas : " + identitas);
+     String x = "Operasi";
+        System.out.println("isi variable x : "+ x);
+        System.out.println("\""+ x + "\"panjang karakter = " + x.length());
         
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variable y : " + y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+           
     }
          
         
