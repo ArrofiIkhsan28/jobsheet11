b94187f8a96288e8e243b8671f202756e40fecf5
Hp <Hp@DESKTOP-GFENN21>
2018-08-16 20:45:08 +0700

    tambah konversi

--- src/jo11/Latiha2.java
+++ src/jo11/Latiha2.java
@@ -17,7 +17,8 @@ public class Latiha2 {
         
         tampiljudul(identitas);
         String kalimat = tampilInput();
-
+        String convert = vocal2Angka(kalimat);
+        
     }
         private static String tampilInput(){
         Scanner ah = new Scanner(System.in);
@@ -26,6 +27,15 @@ public class Latiha2 {
         String kalimat = ah.nextLine();
         System.out.println("Kalimat Asli : "+ kalimat);
         
+        return kalimat;
+    }
+            private static String vocal2Angka(String kalimat){
+        char [][] arConvert = 
+        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i = 0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
         return kalimat;
     }
 }
