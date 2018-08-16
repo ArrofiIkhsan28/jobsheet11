204222e0a2aa47c292936eb42ef1212c78548855
Hp <Hp@DESKTOP-GFENN21>
2018-08-16 20:35:12 +0700

    initia comit

---
+++ src/jo11/Latiha2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jo11;
+
+import java.util.Scanner;
+public class Latiha2 {
+    private static void tampiljudul(String identitas){
+        System.out.println("identitas : "+identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (vokal ke angka) \n");
+    }
+    public static void main(String[] args){
+        String identitas = "arrofi ikhsan nur abror/XRPL 2/08";
+    }
+}
