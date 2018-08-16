f370053996d1ed1334277a57edfcb3b6557fab2c
Hp <Hp@DESKTOP-GFENN21>
2018-08-16 19:06:32 +0700

    initial commit

---
+++ src/jo11/latihan2.java
@@ -0,0 +1,16 @@
+
+
+    package jo11;
+import java.util.Scanner;
+public class latihan2 {
+    private static void tampiljudul(String identitas){
+        System.out.println("identitas : "+identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (vokal ke angka) \n");
+    }
+    public static void main(String[] args){
+        String identitas = "arrofi ikhsan nur abror/XRPL 2/08";
+        tampiljudul(identitas);
+    }
+    
+}
